44735b4d132b7d781a325971beda79c0b43e4adc
kkoischwitz <kolja.koischwitz@akquinet.de>
2018-05-16 06:46:57 +0200

    on push for display field

--- src/app/display-field/display-field.component.ts
+++ src/app/display-field/display-field.component.ts
@@ -1,27 +1,25 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Cell, Operational, Transaction} from 'sodiumjs';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {Cell, Operational} from 'sodiumjs';
+import {BehaviorSubject} from 'rxjs';
 
-// TODO: OnPush + Observable + async im html
 @Component({
   selector: 'app-display-field',
   templateUrl: './display-field.component.html',
-  styleUrls: ['./display-field.component.css']
+  styleUrls: ['./display-field.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DisplayFieldComponent implements OnInit {
 
   @Input() displayC: Cell<number>;
 
-  display: number;
-
-  constructor() { }
+  private displayS = new BehaviorSubject(0);
+  public display = this.displayS.asObservable();
 
   ngOnInit() {
     console.log('Init display field');
-    Transaction.run(() => {
-      Operational
-        .updates(this.displayC)
-        .listen( num => { this.display = num; console.log('received new display value ' + num); });
-    });
+      Operational.updates(this.displayC).listen( num => {
+        this.displayS.next(num);
+      });
   }
 
 }
